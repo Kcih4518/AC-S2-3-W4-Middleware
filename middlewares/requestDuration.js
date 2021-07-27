e3298f0dbe6ea3e11c0a11b205f03b86c83ecae3
Kcih4518 <kcih4518@gmail.com>
2021-07-27 11:55:18 +0800

    <feat>[AC-S2-3-W4-Middleware]: Use fs moduel to save request log into logs/request.txt

--- middlewares/requestDuration.js
+++ middlewares/requestDuration.js
@@ -1,4 +1,5 @@
 const moment = require('moment')
+const fs = require('fs')
 
 function requestDuration(req, res, next) {
   const dateObject = new Date()
@@ -6,8 +7,12 @@ function requestDuration(req, res, next) {
   const dateStringWithTime = moment(dateObject).format('YYYY-MM-DD HH:mm:ss')
   const requestMethod = req.method
   const requestPath = req.url
+  const requestLog = `${dateStringWithTime} | ${requestMethod} from ${requestPath}\n`
 
-  console.log(`${dateStringWithTime} | ${requestMethod} from ${requestPath}`)
+  fs.appendFile('./logs/request.txt', requestLog, (error) => {
+    if (error) console.log(error)
+  })
+
   next()
 }
 
