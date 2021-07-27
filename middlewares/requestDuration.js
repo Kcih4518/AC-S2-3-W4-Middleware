e721db1102bfbe57aa738db325fa421b3272f430
Kcih4518 <kcih4518@gmail.com>
2021-07-27 13:01:17 +0800

    <refactor>[AC-S2-3-W4-Middleware]: Modify dateWithTime to correct location

--- middlewares/requestDuration.js
+++ middlewares/requestDuration.js
@@ -3,14 +3,15 @@ const fs = require('fs')
 
 function requestDuration(req, res, next) {
   const dateObjectStart = new Date()
+  const dateWithTime = moment(dateObjectStart).format('YYYY-MM-DD HH:mm:ss')
   const requestMethod = req.method
   const requestPath = req.url
 
   res.on('finish', () => {
     const dateObjectEnd = new Date()
     const durationTime = dateObjectEnd - dateObjectStart
-    const dateWithTime = moment(dateObjectStart).format('YYYY-MM-DD HH:mm:ss')
     const requestLog = `${dateWithTime} | ${requestMethod} from ${requestPath} | total time: ${durationTime}ms\n`
+
     fs.appendFile('./logs/request.txt', requestLog, (error) => {
       if (error) console.log(error)
     })
