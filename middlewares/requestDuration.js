bc4ca78c21df82c221a28f1984980a603cd3922b
Kcih4518 <kcih4518@gmail.com>
2021-07-27 12:51:41 +0800

    <feat>[AC-S2-3-W4-Middleware]: Update the middleware to calculate the server response time
    1. YYYY-MM-DD HH:mm:ss | request_method from request_url | total time: duration_time ms

--- middlewares/requestDuration.js
+++ middlewares/requestDuration.js
@@ -2,15 +2,18 @@ const moment = require('moment')
 const fs = require('fs')
 
 function requestDuration(req, res, next) {
-  const dateObject = new Date()
-  // use moment
-  const dateStringWithTime = moment(dateObject).format('YYYY-MM-DD HH:mm:ss')
+  const dateObjectStart = new Date()
   const requestMethod = req.method
   const requestPath = req.url
-  const requestLog = `${dateStringWithTime} | ${requestMethod} from ${requestPath}\n`
 
-  fs.appendFile('./logs/request.txt', requestLog, (error) => {
-    if (error) console.log(error)
+  res.on('finish', () => {
+    const dateObjectEnd = new Date()
+    const durationTime = dateObjectEnd - dateObjectStart
+    const dateWithTime = moment(dateObjectStart).format('YYYY-MM-DD HH:mm:ss')
+    const requestLog = `${dateWithTime} | ${requestMethod} from ${requestPath} | total time: ${durationTime}ms\n`
+    fs.appendFile('./logs/request.txt', requestLog, (error) => {
+      if (error) console.log(error)
+    })
   })
 
   next()
